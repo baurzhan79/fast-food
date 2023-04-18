708cb599fad47cfebc4a39ff5c010c93526b6610
Baurzhan Yussupov <baurzhangulnur@gmail.com>
2023-04-18 12:29:37 +0600

    #1 New component Item has been created which displays name, price, icon of the buttons for adding a product

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import Item from './components/Item/Item';
 
 function App() {
+  const items = [
+    { name: "Hamburger", price: 300, iconSrc: "./icons/icon-hamburger.png" },
+    { name: "Hot-dog", price: 280, iconSrc: "./icons/icon-hotdog.png" },
+    { name: "Pizza", price: 350, iconSrc: "./icons/icon-pizza.png" },
+    { name: "Fries", price: 250, iconSrc: "./icons/icon-fries.png" },
+    { name: "Coffee", price: 100, iconSrc: "./icons/icon-coffee.png" },
+    { name: "Tea", price: 80, iconSrc: "./icons/icon-tea.png" }
+  ];
+
+  const addNewItem = (item) => {
+    console.log("item", item);
+  }
+
   return (
     <div className="App">
-
-    </div>
+      {
+        items.map((item, index) => {
+          return (<Item
+            key={index}
+            item={item}
+            onAddItem={() => addNewItem(item)}
+          />);
+        })
+      }
+    </div >
   );
 }
 
---
+++ src/components/Item/Item.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Item.css";
+
+const Item = props => {
+    const iconStyle = {
+        backgroundImage: `url(${props.item.iconSrc})`
+    };
+
+    return (
+        <button className="Item-btn" onClick={props.onAddItem}>
+            <span className="Item-spanBox" style={iconStyle}>
+                <span className="Item-span">{props.item.name}</span>
+                <span className="Item-span">Price: {props.item.price} KZT</span>
+            </span>
+        </button >
+    );
+}
+
+export default Item
